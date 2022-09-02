8f8d2c2eb05c1fb258814b5171668a090a5e2ec9
wudilsr <wudilsr@gmail.com>
2022-09-02 11:18:58 +0800

    hi6250-9-common: Add android.hardware.audio.service

--- hi6250.mk
+++ hi6250.mk
@@ -23,6 +23,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.audio.service_64 \
     android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.audio.service \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.r_submix.default \
