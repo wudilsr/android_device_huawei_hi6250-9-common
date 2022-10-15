5581fb7a5383eecf77c285c7cdb484e4ede54a12
arco <>
2022-10-15 18:49:52 +0800

    hi6250-9-common: Silence clang warnings during kernel compilation clang-14: warning: argument unused during compilation: '-fuse-ld=lld' [-Wunused-command-line-argument]

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,11 +56,10 @@ TARGET_KERNEL_SOURCE := kernel/huawei/hi6250-9
 
 TARGET_KERNEL_OPTIONAL_LD := true
 
-# Clang
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := r416183b1
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
-
+TARGET_KERNEL_CLANG_PATH := $(PWD)/prebuilts/clang/host/linux-x86/clang-r416183b1
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+    
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
